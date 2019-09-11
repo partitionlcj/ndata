6a01385f82a713dcfdb19ddfd44ceea277d42a95
周际 <ji.zhou@megatronix.co>
2019-09-11 18:02:36 +0800

    add cmd scripts

---
+++ scripts/run_di.sh
@@ -0,0 +1,4 @@
+while true; do
+	python3 sync_ri_v2.py
+	sleep 60
+done
---
+++ scripts/ssh_bridgh.sh
@@ -0,0 +1,1 @@
+ssh -o ExitOnForwardFailure=yes -NL 33106:mysql-0.mysql.test-inf.svc.cluster.local:3306 172.23.195.216 -p 3322
